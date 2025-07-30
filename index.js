8b00af0adfcba1eb54bf67d24dfe2d664a3985cf
Irfansadat-ops <irfansadat95@gmail.com>
2025-07-30 20:48:38 +0600

    api updated

--- index.js
+++ index.js
@@ -33,11 +33,64 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server	
     // await client.connect();
 
     const craftcollection = client.db('craftDB').collection('craft');
     const subcategorycollection = client.db('craftDB').collection('categories');
+
+    app.get('/craftlist', async (req, res) => {
+      const cursor = craftcollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // Data collect from craft database by id
+    app.get('/craftlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftcollection.findOne(query);
+      res.send(result);
+    })
+
+    // Data create on craft database
+    app.post('/craftlist', async (req, res) => {
+      const newcraft = req.body;
+      const result = await craftcollection.insertOne(newcraft);
+      res.send(result);
+    })
+
+    // Data updated on craft database
+    app.put('/craftlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedcraft = req.body;
+      const updated = {
+        $set: {
+          title: updatedcraft.title,
+           subcategory: updatedcraft.subcategory,
+           imageurl: updatedcraft.imageurl,
+           description: updatedcraft.description,
+           price: updatedcraft.price,
+           rating: updatedcraft.rating,
+           customization: updatedcraft.customization,
+           stockStatus: updatedcraft.stockStatus,
+           processingtime: updatedcraft.processingtime
+        }
+      } 
+      const result = await craftcollection.updateOne(filter, updated);
+      res.send(result);
+    })
+
+
+    // Data delete in craft database by id
+    app.delete('/craftlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftcollection.deleteOne(query);
+      res.send(result);
+    })
 
     
  
